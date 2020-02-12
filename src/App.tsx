8e29f65b75e95106bb4746f2fe7ab7b199d57345
edwinisnow <edwin04@gmail.com>
2020-02-12 23:12:04 +0000

    Basic Functional Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import { Card } from './components/Card'
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
-          React + Typescript
+      <Card title="Welcome!" />
     </div>
   );
 }
