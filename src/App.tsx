edf5320168f46f115a74a477ab2e6fec0c516bb5
edwinisnow <edwin04@gmail.com>
2020-02-13 21:42:41 +0000

    Card Functional

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { CardSimple } from './components/CardSimple'
 import './App.css';
 import { CardFunctional } from './components/CardFunctional';
+import { Counter } from './components/CounterHooks';
+
+export const LanguageContext = React.createContext({ lang: 'en' })
 
 const App = () => {
   return (
     <div className="App">
-      <CardSimple title="Card Simple" />
+      {/* <CardSimple title="Card Simple" />
       <CardFunctional title="Card Functional" />
+      <Clock />
+      <Article title="TS" price={10} /> */}
+
+      <Counter />
     </div>
   );
 }
